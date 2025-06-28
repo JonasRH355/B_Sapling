57f7e2c6945c5270c026a8bcbf5791d8f4f1bdf6
Jonas <127670304+JonasRH355@users.noreply.github.com>
2025-06-28 20:44:55 +0000

    Created a simple Prompt, where is a loop with a lot IFs

--- main_fs.c
+++ main_fs.c
@@ -1,5 +1,6 @@
 #include "filesystem.h"
-
+#include <stdio.h>
+/*
 int main() {
     // Criação do sistema de arquivos
     Directory* root = get_root_directory();
@@ -33,6 +34,111 @@ int main() {
     } else {
         perror("Erro ao criar fs.img");
     }
+
+    return 0;
+}
+*/ 
+
+char toLower(char c){ // Caso o valor seja uma letra minuscula, será retornadoa em Uppercase
+   if (c >= 'A' && c <= 'Z') {
+      return c + 32;
+   }
+   return c;
+}
+
+// Verifica se as seis primeiras letras são em Uppercase
+void checkIfIsAllLower(char *command){ 
+   for(int i=0; i<6 && command[i] != '\0'; i++){
+      if(command[i] < 'a' || command[i] > 'z'){
+         command[i] = toLower(command[i]);
+      }
+   }
+}
+
+int checkFunction(char *command){
+    char token[5][100];
+    int coluna = 0;
+    int linha = 0;
+
+    for(int i = 0; command[i] != '\0' && command[i] != '\n';i++){
+        if(command[i] == ' '){
+            token[linha][coluna] = '\0';
+            linha++;
+            coluna = 0;
+        }
+        else 
+        {
+            token[linha][coluna] = command[i];
+            coluna++;
+        }
+        
+    }
+    token[linha][coluna] = '\0';
+
+    if (strcmp(token[0], "mkdir")== 0)
+    {
+        printf("Created DIR\n");
+    }
+    else if (strcmp(token[0], "rmdir")== 0)
+    {
+        printf("Remove DIR\n");
+    }
+    else if (strcmp(token[0], "touch")== 0)
+    {
+        printf("Created file\n");
+    }
+    else if (strcmp(token[0], "rm")== 0)
+    {
+        printf("Removed file\n");
+    }
+    else if (strcmp(token[0], "ls")== 0)
+    {
+        printf("List of paths\n");
+    }
+    else if (strcmp(token[0], "cd")== 0)
+    {
+        printf("Go to new path\n");
+    }
+    else{
+        printf("Command not found\n");
+    }
+
+}
+
+
+
+int PROMPT(){
+    char text[100]= "";
+    char dir[100] = "";
+
+    while (1)
+    {
+        printf("->%s $ ",dir);
+        fgets(text, 100, stdin);
+        //scanf("%s",text); //"%d",
+        
+        checkIfIsAllLower(text);
+
+        if(strcmp(text, "")== 0 || strcmp(text, "\n")== 0){
+
+        }
+        else if(strncmp(text, "quit",4)== 0){ //defini por pegar apenas as 4 primeiras letras ao inves de ter que verificar "quit\n"
+            printf("Closing PROMPT\n");
+            break;
+        }
+        else {
+            checkFunction(text);
+        }
+    }
+    
+    return 0;
+}
+
+
+
+
+int main(){
+    PROMPT();
 
     return 0;
 }
